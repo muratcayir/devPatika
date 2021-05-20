f4803a965fa767f89413f49b08e6d2e3631e6e24
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-05-20 14:02:56 +0300

    Homewrok3 eklendi

---
+++ NodeJS/Homework3/circle.js
@@ -0,0 +1,17 @@
+const  circleArea =(r)=>
+{
+
+    console.log(r)
+}
+
+const  circleCircumference=(r)=>
+{
+
+    console.log(r)
+}
+
+
+module.exports ={
+    circleArea,
+    circleCircumference
+}
